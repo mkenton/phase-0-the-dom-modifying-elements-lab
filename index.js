7ff8da9b4403222ab5213382a4cbd95eaf32119b
Michael <mkenton2020@icloud.com>
2021-07-12 16:09:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "MICHAEL is the champion";
+document.body.append(newHeader);
+
